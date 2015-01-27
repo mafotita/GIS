f3a74a7523820cffa9b8b744e29aee94d9894125
Ross Wardrup <minorsecond@gmail.com>
2015-01-26 19:05:06 -0600

    Added quite a bit of code. Mostly to catch missing files and proections.

--- tifFix.py
+++ tifFix.py
@@ -5,29 +5,62 @@
 
 from osgeo import gdal,ogr,osr
 import os
+#from __future__ import print_function
 gdal.UseExceptions()
 
-  """
-  The projCheck function will iterate through input directory and make sure files are in correct proection.
-  This should be run on both pre and post processed images to double-check integrity.
-  I'm pretty sure the GetProjection function is the incorrect one to use but it's pretty much a
-  placeholder for now.
-  """
+  #The projCheck function will iterate through input directory and make sure files are in correct proection.
+  #This should be run on both pre and post processed images to double-check integrity.
+  #I'm pretty sure the GetProjection function is the incorrect one to use but it's pretty much a
+  #placeholder for now.
+
 def projCheck(r):
   if not r.GetProjection is WGS84:
     print "file %s is not in WGS84 projection. Cannot continue." %r
-    sys.exit()
+    sys.exit(1)
+  elif r.GetProe is None and geotransform is None:
+    print "No projection found in %s" %r
+    sys.exit(1)
+
+
 ### This should take an input file and check whether or not it already is assigned a projection. If coordinates
 ### are not found, then coordinates of a matching filename are applied.
 
-def applyRefs(input, outDir):
+def applyRefs(input):
   print "processing %s" %input
   in_file = gdal.open(input)
   gt = in_file.GetGeoTransform()
-  if not geotransform is None:
-    print 'Origin = (',geotransform[0], ',',geotransform[3],')'
+
+  if geotransform is not None:
+    x, x_size, x_rot, y, y_rot, y_size = geotransform
+    world_file.write('%s\n' % x_size)
+    world_file.write('%s\n' % x_rot)
+    world_file.write('%s\n' % y_rot)
+    world_file.write('%s\n' % y_size)
+    world_file.write('%s\n' % x)
+    world_file.write('%s\n' % y)
+    world_file.close()
+
+    ''' geotransform tuple key:
+    [0] /* top left x */
+    [1] /* w-e pixel resolution */
+    [2] /* rotation, 0 if image is "north up" */
+    [3] /* top left y */
+    [4] /* rotation, 0 if image is "north up" */
+    [5] /* n-s pixel resolution */
+    '''
   else:
-    print" this is where the function to copy world file info to processed tif will go"
+    print "sucks to be you."
+
+tifs = []
+
+def srchFl(dir):
+  if not os.path.exists(dir):
+    print "directory %s does not exist. Exiting" %dir
+    sys.exit()
+  else:
+    for file in os.listdir(dir):
+      if file.endswith(".tif"):
+        tifs.extend(file)
 
 preDir = raw_input("Please enter the directory in which the pre-processed GTIF files reside: ")
 postDir = raw_input("Please enter the directory in which the post-processed GTIF files reside: ")
