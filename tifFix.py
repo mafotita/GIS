9de31459a0056fd7819e4dd479693ef6d349a2f8
Ross Wardrup <minorsecond@gmail.com>
2015-01-26 18:08:47 -0600

    Added some text

--- tifFix.py
+++ tifFix.py
@@ -3,17 +3,22 @@
 ### Script to reapply geographic coordinates from preprossed to postprocessed TIF images.
 ### This script requires GDAL to run. ArcPy is not used due to licensing issues.
 
-import gdal
+from osgeo import gdal,ogr,osr
 import os
+gdal.UseExceptions()
 
+  """
+  This function will iterate through input directory and make sure files are in correct proection.
+  This should be run on both pre and post processed images to double-check integrity.
+  I'm pretty sure the GetProjection function is the incorrect one to use but it's pretty much a
+  placeholder for now.
+  """
 def projCheck(r):
-  #This function will iterate through input directory and make sure files are in correct proection.
-  #This should be run on both pre and post processed images to double-check integrity.
   if not r.GetProjection is WGS84:
     print "file %s is not in WGS84 projection. Cannot continue." %r
     sys.exit()
 
-def applyRefs(input, output):
+def applyRefs(input, outDir):
   print "processing %s" %input
   in_file = gdal.open(input)
   gt = in_file.GetGeoTransform()
@@ -24,6 +29,7 @@ def applyRefs(input, output):
 
 preDir = raw_input("Please enter the directory in which the pre-processed GTIF files reside: ")
 postDir = raw_input("Please enter the directory in which the post-processed GTIF files reside: ")
+outDir = raw_input("Please enter the directory in which the final, fixed GTIF files should be placed: ")
 raw_input("GeoTifs should be projected in WGS84. Press Enter to check projection of input files.")
 
 for r in preDir:
